733dfe865a7073eb7d194841a1ed6421fc04db6c
MD Waliullah <waliullah.godev@gmail.com>
2025-08-13 18:06:30 +0000

    successfully create a GO server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gowaliullah/blog-rest-api
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+var welcomeHandler = func(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome to my blog server API, thanks for visiting")
+}
+var aboutHandler = func(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Our purpose to service real world news supply")
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", welcomeHandler)
+	mux.HandleFunc("/about", aboutHandler)
+
+	fmt.Println("Server running on PORT: 8080")
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		fmt.Println("Error starting the server", err)
+	}
+
+}
