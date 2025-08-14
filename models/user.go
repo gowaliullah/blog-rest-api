1b600b9ff68372135002a33605d600cfdc517da6
MD Waliullah <waliullah.godev@gmail.com>
2025-08-14 15:21:57 +0000

    create post model and update user model

--- models/user.go
+++ models/user.go
@@ -1,8 +1,9 @@
 package models
 
 type User struct {
-	ID       int    `json:"_id"`
-	Name     string `json:"title"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	ID       int     `json:"_id"`
+	Name     string  `json:"title"`
+	Email    string  `json:"email"`
+	Password string  `json:"password"`
+	Photo    *string `json:"photo"`
 }
