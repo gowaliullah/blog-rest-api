6ec35680ff63ab602fed2af75aae6195a91d0293
MD Waliullah <waliullah.godev@gmail.com>
2025-08-20 11:45:22 +0000

    user models create

--- models/user.go
+++ models/user.go
@@ -1,9 +1,33 @@
 package models
 
+import (
+	"time"
+)
+
+type UserRole string
+
+const (
+	RoleAdmin  UserRole = "admin"
+	RoleAuthor UserRole = "author"
+	RoleReader UserRole = "reader"
+)
+
 type User struct {
-	ID       int     `json:"_id"`
-	Name     string  `json:"title"`
-	Email    string  `json:"email"`
-	Password string  `json:"password"`
-	Photo    *string `json:"photo,omitempty"`
+	UserID    int       `json:"user_id"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	UserRole  UserRole  `json:"user_role"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
+
+// type User struct {
+// 	UserID    int       `db:"user_id" json:"user_id"`
+// 	Username  string    `db:"username" json:"username"`
+// 	Email     string    `db:"email" json:"email"`
+// 	Password  string    `db:"password" json:"password"`
+// 	UserRole  UserRole  `db:"user_role" json:"user_role"`
+// 	CreatedAt time.Time `db:"created_at" json:"created_at"`
+// 	UpdatedAt time.Time `db:"updated_at" json:"updated_at"`
+// }
