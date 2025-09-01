14aa72dc6de9484baf6fc542b2c4c342dae3937a
MD Waliullah <waliullah.godev@gmail.com>
2025-09-01 14:59:25 +0600

    SendError util func for through error

---
+++ util/send_error.go
@@ -0,0 +1,12 @@
+package util
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func SendError(w http.ResponseWriter, statusCode int, msg string) {
+	w.WriteHeader(statusCode)
+	encoder := json.NewEncoder(w)
+	encoder.Encode(msg)
+}
