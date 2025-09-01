c7fb56158e130fa5a9c6709d1188c4332efd1c07
MD Waliullah <waliullah.godev@gmail.com>
2025-09-01 13:55:53 +0600

    laod all env file using "godotenv"

--- config/config.go
+++ config/config.go
@@ -17,6 +17,33 @@ type Config struct {
 	HttpPort    int
 }
 
+func loadConfig() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("fail to load .env variables:", err)
+		os.Exit(1)
+	}
+
+	version := os.Getenv("VERSION")
+	if version == "" {
+		log.Fatal("VERSION is required")
+		os.Exit(1)
+	}
+
+	serviceName := os.Getenv("SERVICE_NAME")
+	if serviceName == "" {
+		log.Fatal("SERVICE_NAME is required")
+		os.Exit(1)
+	}
+
+	httpPort := os.Getenv("HTTP_PORT")
+	if httpPort == "" {
+		log.Fatal("HTTP_PORT is required")
+		os.Exit(1)
+	}
+
+}
+
 func ConnectDB() {
 
 	// Load the .env file
